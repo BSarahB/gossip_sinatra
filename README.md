1f02ebfc53f607214e6c24217dd0046987e10363
BSarahB <bslajta@gmail.com>
2019-10-21 22:17:54 +0200

    adding files

---
+++ README.md
@@ -0,0 +1,16 @@
+Contributor @SarahB Toulouse 2019
+
+The Hacking Project is a great training, where the community is queen. There is a lot going on in Paris, Lyon, Montpellier, Bordeaux and it's hard to know all the gossip. So we will create an application where everyone will be able to add gossip on his schoolmates comrades.
+
+In outline, here are the main features of our web application:
+
+    The home page of the site will show all the gossip that we have in base.
+    This homepage will give a link for a form where anyone can add a gossip to the database.
+    Each gossip will have a dedicated page.
+
+This ruby app is based on the framework Sinatra. we follow a MVC Model View Controller architecture to organize the app.
+
+
+
+
+
---
+++ lib/gossip.rb
@@ -0,0 +1,41 @@
+require 'csv'
+
+class Gossip
+	attr_accessor :content, :author
+
+  def initialize(author, content)
+
+    @content = content
+	  @author = author
+  end
+    
+
+  def save_as_CSV
+
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+	   csv << [@author, @content]
+
+    end  
+  end
+  
+  def self.all
+  # 1)création d'un array vide qui s'appelle all_gossips
+      all_gossips = []
+  #2)chaque ligne de ton CSV.each do |ligne|
+      file = CSV.read("./db/gossip.csv")
+      file.each do |ligne|
+ 	#ligne nous renvoie un array avec les 2 strings du gossip ["sarah", "pas de potins"]
+      gossip_provisoire = Gossip.new(ligne[0],ligne[1])	
+      all_gossips << gossip_provisoire
+      end
+      	
+  # 3)return all_gossips - on renvoie le résultat
+  return all_gossips
+end
+ 
+  def self.find(id)
+    id = id.to_i # on convertit la string en integer pour pouvoir renseigner l index de l array just en dessous
+    return self.all[id] #self.all n est autre que Gossip.all ainsi on prend l index de l array
+  end
+
+end
